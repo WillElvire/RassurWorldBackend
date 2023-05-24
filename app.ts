69485512456a5c7e039c857b8c9e00cb2ddc46d4
macbook <wkoua@oxylead.net>
2023-05-24 20:09:48 +0000

    feat : file uploads

--- app.ts
+++ app.ts
@@ -1,7 +1,13 @@
 import boostraping from "./src";
 import  express  from 'express';
 import * as dotenv from 'dotenv';
+import path from "path";
 const app = express(); 
+const multer = require("multer");
 dotenv.config()
+
+
+
+app.use(express.static(path.join("./public/")));
 boostraping.init(app,3002);
 export default app;
