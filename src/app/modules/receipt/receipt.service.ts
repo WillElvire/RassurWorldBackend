25363304bd2234ce03d241c10ed56c2394d19391
macbook <wkoua@oxylead.net>
2023-07-14 08:55:23 +0000

    feat : adding receipt  management to backend

--- src/app/modules/receipt/receipt.service.ts
+++ src/app/modules/receipt/receipt.service.ts
@@ -1,3 +1,27 @@
+import { ReceiptDto } from "./dto/receipt.dto";
+import { ReceiptPersistence } from "./receipt.persitence";
+
+
 export class ReceiptService {
+    private receiptPersistence  =  new ReceiptPersistence();
+   
+    async save(role : ReceiptDto)  {
+       return this.receiptPersistence.save(role);
+    }
+
+    findAll(){
+        return this.receiptPersistence.find();
+    }
     
+    findRoleById() {
+
+    }
+
+    findReceiptById(id) {
+        return this.receiptPersistence.getReceiptByid(id);
+    }
+
+    findRoleByFlag() {
+
+    }
 }
